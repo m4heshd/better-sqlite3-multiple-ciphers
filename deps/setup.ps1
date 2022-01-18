3d95e44608c331bdbb740a0b95bfae8b284ea970
Mahesh Bandara Wijerathna <m4heshd@gmail.com>
2022-01-19 02:06:46 +0530

    Remove `tar` dependency and update scripts, sources
    Other changes:
    - Use `files` property on `package.json` to avoid publishing unnecessary files with the package
    
    Co-authored-by: Joshua Wise <joshuathomaswise@gmail.com>

--- deps/setup.ps1
+++ deps/setup.ps1
@@ -9,7 +9,7 @@ $API_URL = "https://api.github.com/repos/utelle/SQLite3MultipleCiphers/releases/
 $TEMP_PATH = Join-Path $PSScriptRoot "temp"
 $SOURCE_PATH = Join-Path $TEMP_PATH "source.zip"
 $EXTRACT_PATH = Join-Path $TEMP_PATH "extracted"
-$TAR_PATH = Join-Path $PSScriptRoot "sqlite3.tar.gz"
+$DEST_PATH = Join-Path $PSScriptRoot "sqlite3"
 
 Write-Host "Preparing.."
 Remove-Item -Path $TEMP_PATH -Recurse -Force -ErrorAction Ignore
@@ -23,16 +23,14 @@ Write-Host "`r`nDownloading.. ($( $FILE ))"
 Invoke-WebRequest -Uri $DL_URL -OutFile (New-Item -Path $SOURCE_PATH -Force)
 
 Write-Host "`r`nExtracting archive.."
-Expand-Archive  -Path $SOURCE_PATH -DestinationPath $EXTRACT_PATH
+Expand-Archive -Path $SOURCE_PATH -DestinationPath $EXTRACT_PATH
 
-Write-Host "`r`nPreparing source files.."
-Remove-Item -Path (Join-Path $EXTRACT_PATH "sqlite3.h")
-Rename-Item -Path (Join-Path $EXTRACT_PATH "sqlite3mc_amalgamation.h") -NewName "sqlite3.h"
-Rename-Item -Path (Join-Path $EXTRACT_PATH "sqlite3mc_amalgamation.c") -NewName "sqlite3.c"
-
-Write-Host "`r`nCreating tarball.."
-Remove-Item -Path $TAR_PATH -Force
-tar czf $TAR_PATH -C $EXTRACT_PATH sqlite3.c sqlite3.h sqlite3ext.h
+Write-Host "`r`nCopying SQLite3MC source files.."
+Remove-Item -Path $DEST_PATH -Recurse -Force -ErrorAction Ignore
+New-Item $DEST_PATH -Type Directory
+Copy-Item -Path (Join-Path $EXTRACT_PATH "sqlite3mc_amalgamation.h") -Destination (Join-Path $DEST_PATH "sqlite3.h")
+Copy-Item -Path (Join-Path $EXTRACT_PATH "sqlite3mc_amalgamation.c") -Destination (Join-Path $DEST_PATH "sqlite3.c")
+Copy-Item -Path (Join-Path $EXTRACT_PATH "sqlite3ext.h") -Destination (Join-Path $DEST_PATH "sqlite3ext.h")
 
 Write-Host "`r`nCleaning up.."
 Remove-Item -Path $TEMP_PATH -Recurse -Force
