b2a70fb62bf8f33adfc26dfe04d8d9d21fc832f8
Mahesh Bandara Wijerathna <m4heshd@gmail.com>
2022-03-17 05:04:48 +0530

    Update `SQLite3MultipleCiphers` amalgamation to `v1.3.9` (#13)

--- deps/setup.ps1
+++ deps/setup.ps1
@@ -2,7 +2,7 @@
 $ErrorActionPreference = "Stop"
 
 # SQLite Info
-$SQLITEMC_VER = "v1.3.8"
+$SQLITEMC_VER = "v1.3.9"
 $API_URL = "https://api.github.com/repos/utelle/SQLite3MultipleCiphers/releases/tags/" + $SQLITEMC_VER
 
 # Paths
