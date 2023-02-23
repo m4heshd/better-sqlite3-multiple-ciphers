7519632bef841b4a6e2e69ceef1730a8bf4eb975
m4heshd <m4heshd@users.noreply.github.com>
2023-02-23 19:17:50 +0000

    Update `SQLite3MultipleCiphers` amalgamation to `v1.6.0`

--- deps/setup.ps1
+++ deps/setup.ps1
@@ -2,7 +2,7 @@
 $ErrorActionPreference = "Stop"
 
 # SQLite Info
-$SQLITEMC_VER = "v1.5.5"
+$SQLITEMC_VER = "v1.6.0"
 $API_URL = "https://api.github.com/repos/utelle/SQLite3MultipleCiphers/releases/tags/" + $SQLITEMC_VER
 
 # Paths
