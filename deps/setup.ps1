69384b6819464ffe7ce2fbe146a20957bdfe5f2a
m4heshd <m4heshd@users.noreply.github.com>
2023-11-23 22:56:46 +0000

    Update `SQLite3MultipleCiphers` to `v1.8.0` (SQLite `3.44.1`)

--- deps/setup.ps1
+++ deps/setup.ps1
@@ -2,7 +2,7 @@
 $ErrorActionPreference = "Stop"
 
 # SQLite Info
-$SQLITEMC_VER = "v1.7.4"
+$SQLITEMC_VER = "v1.8.0"
 $API_URL = "https://api.github.com/repos/utelle/SQLite3MultipleCiphers/releases/tags/" + $SQLITEMC_VER
 
 # Paths
